6b5662c4e5fdebadfa1034a3f85a5907092120c2
RomainBenatar <119604047+RomainBenatar@users.noreply.github.com>
2023-12-05 14:24:52 +0100

    Finish challenge 2

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -7,6 +7,8 @@ currentId = 0
 colours = ['blue','green','red']
 colours_max = [14,13,12]
 current_colours = [0,0,0]
+current_max_colours = [0,0,0]
+finalPower = 0
 while len(currentLine) > 0:
 	numberIndex=5
 	currentId=currentLine[numberIndex]
@@ -21,16 +23,21 @@ while len(currentLine) > 0:
 	for a in currentLineSplit:
 		j = 0
 		for c in colours:
-			if a ==";":
+			if a == "Game":
+				current_max_colours[j]=0
+			if a ==";" or a == "Game":
 				current_colours[j] = 0
 			if c == a:
 				current_colours[j] += int(currentLineSplit[indexTable-1])
 			if int(current_colours[j])>int(colours_max[j]):
 				possible=False
+			if int(current_colours[j])>current_max_colours[j]:
+				current_max_colours[j] = current_colours[j]
 			j += 1
 		indexTable +=1
 	if possible:
 		finalSum += int(currentId)
+	finalPower+=current_max_colours[0]*current_max_colours[1]*current_max_colours[2]
 	currentLine = myInput.readline()
 print (finalSum)
-	
+print (finalPower)
